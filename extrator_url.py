298d833861d4486b29ad5255bd79ccef4260663d
adrianohenriquess <adriano.henriquesds@gmail.com>
2023-10-19 21:36:06 -0300

    Adicionando métodos especiais eq, str, e len para a classe extrator_url

--- extrator_url.py
+++ extrator_url.py
@@ -38,7 +38,25 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
 
-extrator_url = ExtratorURL("https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real")
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+    def __str__(self):
+        return (self.url + "\n" + "Parâmetros: " + self.get_url_parametros()
+                                    + "\n" + "URL Base" + self.get_url_base())
+
+    def __eq__(self, other):
+        return self.url == other.url
+
+
+url = "https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
+extrator_url_1 = ExtratorURL(url)
+extrator_url_2 = ExtratorURL(url)
+print("O tamanho da URL é: ", len(extrator_url_1))
+print(extrator_url_1)
+valor_quantidade = extrator_url_1.get_valor_parametro("quantidade")
 print(valor_quantidade)
+
+print(id(extrator_url_1))
+print(id(extrator_url_2))
+print(extrator_url_1 == extrator_url_2)
